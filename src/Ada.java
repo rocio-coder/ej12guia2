9f30b336ed98ebc0372cf9a7456c1a46b6b45c80
rocio-corder <suarez.rocio96@gmail.com>
2019-10-11 21:14:14 -0300

    Initial commit

---
+++ src/Ada.java
@@ -0,0 +1,40 @@
+
+import java
+
+import java.util.Scanner;
+
+public class Ada {
+    public static void main(String[] args) {
+
+        int nc,tot;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("ingrese el numero de cita: ");
+
+        switch (nc) (
+        case "1":
+        if nc<= 3;
+        tot= 200*nc;
+    )else (
+            case 2:
+            nc<=5;
+            tot= (nc-3) * 150 + 600;
+        ) else(
+            case 3:
+            nc<= 8;
+                  tot= (nc-5)* 100+ 900;
+        ) else(
+            case 4:
+            nc=50;
+                    tot= (nc-8)*50+ 1200;)
+
+            default
+                        System.out.print("no paga");
+        int d = scanner.nextint();
+
+
+
+    }
+
+}
